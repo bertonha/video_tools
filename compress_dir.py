d6aecfda1907e08af4ed9de7fb9e5a359d12c79e
Christofer Bertonha <christoferbertonha@gmail.com>
2019-08-13 00:02:06 +0200

    Add setup.py

--- compress_dir.py
+++ compress_dir.py
@@ -8,7 +8,7 @@ from pathlib import Path
 @click.command()
 @click.option("--delete", default=False, is_flag=True)
 @click.argument("initial_path")
-def main(initial_path, delete):
+def cli(initial_path, delete):
     p = Path(initial_path)
     for in_filename in p.glob("**/*.mp4"):
         out_filename = "{}_compressed.mp4".format(str(in_filename)[:-4])
@@ -25,4 +25,4 @@ def main(initial_path, delete):
 
 
 if __name__ == "__main__":
-    main()
+    cli()
---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='Video Compress',
+    version='0.1',
+    py_modules=['compress_dir'],
+    install_requires=[
+        "Click",
+        "pathlib",
+    ],
+    entry_points='''
+        [console_scripts]
+        compress_dir=compress_dir:cli
+    ''',
+)
