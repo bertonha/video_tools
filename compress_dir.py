4729d3d303f5989a44bb1a318baad44bb4c1ae6b
Christofer Bertonha <christoferbertonha@gmail.com>
2019-08-12 23:53:16 +0200

    add delete option

--- compress_dir.py
+++ compress_dir.py
@@ -1,3 +1,4 @@
+import os
 import subprocess
 
 import click
@@ -5,8 +6,9 @@ from pathlib import Path
 
 
 @click.command()
+@click.option("--delete", default=False, is_flag=True)
 @click.argument("initial_path")
-def main(initial_path):
+def main(initial_path, delete):
     p = Path(initial_path)
     for in_filename in p.glob("**/*.mp4"):
         out_filename = "{}_compressed.mp4".format(str(in_filename)[:-4])
@@ -17,7 +19,9 @@ def main(initial_path):
             "-crf", "28",
             "-c:a", "aac", "-b:a", "128k",
             out_filename,
-        ])
+        ], check=True)
+        if delete:
+            os.remove(str(in_filename))
 
 
 if __name__ == "__main__":
