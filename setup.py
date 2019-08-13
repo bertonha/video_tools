f477d451ccf8cb14c83d6c8da8d3244a233a7596
Christofer Bertonha <christofer.bertonha@chartboost.com>
2019-08-13 17:29:08 +0200

    files and dirs

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import setup
 setup(
     name='Video Compressor',
     version='0.1',
-    py_modules=['compress_dir'],
+    py_modules=['video_compress'],
     install_requires=[
         "Click",
         "pathlib",
     ],
     entry_points='''
         [console_scripts]
-        compress_dir=compress_dir:cli
+        video_compress=video_compress:cli
     ''',
 )
--- video_compress.py
+++ video_compress.py
@@ -8,11 +8,22 @@ from pathlib import Path
 OUTPUT_SUFFIX = "_compressed.mp4"
 
 
-def is_meta_file(in_filename):
-    return in_filename.startswith("._")
+def is_meta_file(filename):
+    return filename.startswith("._")
 
 
-def compress_file(in_filename):
+def compress_file(file_, delete):
+    in_filename = str(file_)
+    if in_filename.endswith(OUTPUT_SUFFIX):
+        return
+    elif is_meta_file(file_.name):
+        return
+    call_ffmpeg(in_filename)
+    if delete:
+        os.remove(in_filename)
+
+
+def call_ffmpeg(in_filename):
     out_filename = "{}{}".format(in_filename[:-4], OUTPUT_SUFFIX)
     subprocess.run(
         [
@@ -28,19 +39,15 @@ def compress_file(in_filename):
 
 @click.command()
 @click.option("--delete", default=False, is_flag=True)
-@click.option("--input_ext", default="mp4")
+@click.option("--input_ext", default="MP4")
 @click.argument("initial_path")
 def cli(initial_path, delete, input_ext):
     p = Path(initial_path)
-    for file_ in p.glob("**/*.{}".format(input_ext)):
-        in_filename = str(file_)
-        if in_filename.endswith(OUTPUT_SUFFIX):
-            continue
-        elif is_meta_file(in_filename):
-            continue
-        compress_file(in_filename)
-        if delete:
-            os.remove(in_filename)
+    if p.is_dir():
+        for file_ in p.glob("**/*.{}".format(input_ext)):
+            compress_file(file_, delete)
+    else:
+        compress_file(p, delete)
 
 
 if __name__ == "__main__":
