d35bab365dc0c027c61110c7c557508dccd6ffe5
Christofer Bertonha <christoferbertonha@gmail.com>
2019-08-31 00:15:34 +0200

    refactor count

--- video_tools/count.py
+++ video_tools/count.py
@@ -1,3 +1,4 @@
+from collections import Counter
 from pathlib import Path
 
 import click
@@ -11,13 +12,20 @@ def count_group():
 
 
 @count_group.command()
-@click.argument("initial_path")
-@click.argument("extension")
-@click.option("--verbose", default=False, is_flag=True)
-def count(initial_path, extension, verbose):
+@click.option("-v", "--verbose", default=False, is_flag=True)
+@click.argument("initial_path", type=click.Path(exists=True))
+@click.argument("extensions", required=False, nargs=-1)
+def count(initial_path, extensions, verbose):
     p = Path(initial_path)
-    files = list(p.glob(f"**/*.{extension}"))
-    print_style(f"Total {extension} files: {len(files)}")
-    if verbose:
-        for file_ in files:
-            print_style(str(file_), fg="blue")
+    counter = Counter()
+    for file_ in p.glob(f"**/*.*"):
+        counter[file_.suffix.lower()] += 1
+
+    if not extensions:
+        extensions = list(counter.keys())
+
+    for extension in extensions:
+        print_style(f"Total {extension} files: {counter[extension.lower()]}")
+        if verbose:
+            for file_ in p.glob(f"**/*{extension}"):
+                print_style(str(file_), fg="blue")
