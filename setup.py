745e54a2227450c7ff873df11b7e05d83fb0fd25
Christofer Bertonha <christofer.bertonha@chartboost.com>
2019-09-04 14:56:23 +0200

    Added base for tests

--- setup.py
+++ setup.py
@@ -2,16 +2,13 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name='Video Tools',
-    version='0.1',
+    name="Video Tools",
+    version="0.1",
     packages=find_packages(),
-    include_package_data=True,
-    install_requires=[
-        "Click",
-        "colorama",
-    ],
-    entry_points='''
-        [console_scripts]
-        video_tools=video_tools.__main__:cli
-    ''',
+    install_requires=["Click>=7.0.0", 'colorama;sys_platform=="win32"'],
+    classifiers=["Programming Language :: Python :: 3"],
+    license="MIT",
+    tests_require=["pytest"],
+    setup_requires=["pytest-runner"],
+    entry_points={"console_scripts": ["video_tools=video_tools.__main__:cli"]},
 )
